aee39beb4af05a8d8c42d7ad7912f88ccb7122b4
Noura Kaddoura <nourakaddoura@gmail.com>
2019-09-16 18:11:43 -0700

    class 10 variables  and data types beginning

---
+++ 1_variables/js/index.js
@@ -0,0 +1,11 @@
+//Declare & Assign Variables
+var name = "";
+var age = 0;
+var random_fact = "";
+var funny_fact = "";
+var show_funny_fact = true;
+
+//Access Variables
+$('#name').html("no answer");
+$('#age').html("no answer");
+$('#fact').html("no answer");
